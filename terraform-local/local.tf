03f8a2345c88a3bf8f09167b208e4b9751f55cff
Ubuntu <ubuntu@ip-172-31-46-121.ap-south-1.compute.internal>
2025-09-15 10:27:14 +0000

    Initial commit

---
+++ terraform-local/local.tf
@@ -0,0 +1,16 @@
+resource "local_file" "devops" {
+        filename = "/home/ubuntu/terraform-learning/terraform-local/devops_automated.txt"
+        content = "I want to become a DevOps Engineer who knows Terraform"
+}
+
+
+resource "random_string" "rand_str" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "rand_str" {
+  value = random_string.rand_str.result
+}
+
